283d0b1099814a2c9c65e66e045474a13671f39e
Quilch <Quillich775@gmail.com>
2015-10-12 23:29:18 +0500

    first release

--- phoneBook.js
+++ phoneBook.js
@@ -1,52 +1,64 @@
 'use strict';
 
-var phoneBook; // Здесь вы храните записи как хотите
+var phoneBook = [];
 
-/*
-   Функция добавления записи в телефонную книгу.
-   На вход может прийти что угодно, будьте осторожны.
-*/
 module.exports.add = function add(name, phone, email) {
-
-    // Ваша невероятная магия здесь
-
+    if (validPhone(phone) && validMail(email)) {
+        var tmp = {
+            name: name,
+            phone: phone,
+            email: email
+        };
+    }
+    phoneBook.push(tmp);
 };
 
-/*
-   Функция поиска записи в телефонную книгу.
-   Поиск ведется по всем полям.
-*/
+function validPhone(phone) {
+    var phoneRegex = /^(\+?\d{1,2})?\s?(\(\d{3}\)|(\d{3}))\s?(\d{3})(\s|-)?(\d{1})(\s|-)?(\d{3})$/;
+    return phoneRegex.test(phone);
+}
+
+function validMail(email) {
+    var mailRegex = /^(\w)+@[A-Za-zА-Яа-я0-9_-]+\.[A-Za-zА-Яа-я0-9_]+(\.([A-Za-zА-Яа-я0-9_])+)?/;
+    return mailRegex.test(email);
+}
+
 module.exports.find = function find(query) {
-
-    // Ваша удивительная магия здесь
-
+    if (query == null) {
+        for (var i = 0; i < phoneBook.length; i++) {
+            console.log(phoneBook[i].name + ', ' + phoneBook[i].phone + ', ' + phoneBook[i].email);
+        }
+    } else {
+        var telephoneRegex = /\+|\s|-|\(|\)/g;
+        for (var i = 0; i < phoneBook.length; i++) {
+            if (phoneBook[i].name.indexOf(query) != -1 ||
+                phoneBook[i].phone.replace(telephoneRegex, '').indexOf(query) != -1 ||
+                phoneBook[i].email.indexOf(query) != -1) {
+                console.log(phoneBook[i].name +
+                    ', ' + phoneBook[i].phone +
+                    ', ' + phoneBook[i].email);
+            }
+        }
+    }
 };
 
-/*
-   Функция удаления записи в телефонной книге.
-*/
 module.exports.remove = function remove(query) {
-
-    // Ваша необьяснимая магия здесь
-
-};
-
-/*
-   Функция импорта записей из файла (задача со звёздочкой!).
-*/
-module.exports.importFromCsv = function importFromCsv(filename) {
-    var data = require('fs').readFileSync(filename, 'utf-8');
-
-    // Ваша чёрная магия:
-    // - Разбираете записи из `data`
-    // - Добавляете каждую запись в книгу
-};
-
-/*
-   Функция вывода всех телефонов в виде ASCII (задача со звёздочкой!).
-*/
-module.exports.showTable = function showTable() {
-
-    // Ваша чёрная магия здесь
-
+    var amount = 0;
+    var oldBook = phoneBook;
+    phoneBook = [];
+    var telephoneRegex = /\+|\s|-|\(|\)/g;
+    for (var i = 0; oldBook[0] != null; i++) {
+        console.log(oldBook[0]);
+        if (oldBook[0].name.indexOf(query) != -1 ||
+            oldBook[0].phone.replace(telephoneRegex, '').indexOf(query) != -1 ||
+            oldBook[0].email.indexOf(query) != -1) {
+            oldBook.shift();
+            amount++;
+        } else {
+            if (oldBook[0] != null) {
+                phoneBook[i - amount] = oldBook.shift();
+            }
+        }
+    }
+    console.log('Recods deleted: ' + amount);
 };
